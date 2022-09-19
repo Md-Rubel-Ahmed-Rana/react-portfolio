25361752716c8b82f0f0304dec9d6b4abdb02ee8
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-20 02:39:01 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Experience from "./experience/experience";
 import Navbar from "./header/nav";
+import Footer from "./footer/footer";
 function App() {
   return (
     <div>
       <Navbar />
       < Experience />
+      < Footer />
     </div>
   );
 }
---
+++ src/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./footer.css"
+const Footer = () => {
+  return (
+    <footer>
+        <p> &copy; All rights reserved by Md Rubel Ahmed Rana 2022.</p>
+    </footer>
+  )
+}
+
+export default Footer
