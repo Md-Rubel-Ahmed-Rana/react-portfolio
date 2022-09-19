893e7a74875fd2de69ae4947c7a7fb011953378b
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-19 17:56:16 +0600

    Bnnaer section completed

--- src/header/nav.js
+++ src/header/nav.js
@@ -2,28 +2,42 @@ import React from 'react'
 import "./nav.css"
 import logo from "./logo.jpg"
 import collapseImage from "./collapse.jpg"
+import banner from "./banner.png"
 
 function Navbar() {
   return (
     <header>
           <div className='container'>
-              <div className='logo'>
-                  <img src={logo} alt='logo' />
-                  <h2>Md Rubel Ahmed Rana</h2>
-              </div>
-              <div>
-                  <ul className='navbar-disc'>
-                      <li> Home </li>
-                      <li> About </li>
-                      <li> Services </li>
-                      <li> Blogs </li>
-                  </ul>
-                  <div className='collapse' id='collapse'>
-                      <img  src={collapseImage} alt='logo' />
+              <nav className='container-wrapper'>
+                  <div className='logo'>
+                      <img src={logo} alt='logo' />
+                      <h2>Md Rubel Ahmed Rana</h2>
+                  </div>
+                  <div>
+                      <ul className='navbar-disc'>
+                          <li> Home </li>
+                          <li> About </li>
+                          <li> Services </li>
+                          <li> Blogs </li>
+                      </ul>
+                      <div className='collapse' id='collapse'>
+                          <img src={collapseImage} alt='logo' />
+                      </div>
+                  </div>
+              </nav>
+          </div>
+              <div className='banner'>
+                  <div className='banner-info'>
+                      <h3>Future Web Developer</h3>
+                      <h3>About Me</h3>
+                      <p>
+                          Hi there, as you see my portfolio you might be understood that I am a Web Developer. I always try to do something to my best. As a web developer, at first I design a UI (user interface) with HTML and CSS and then will convert it to a demandable Website , Web Application or Desktop Application as the client or company requirement by using the most popular technology ( Vanilla JavaScript, Reactjs, TypeScript, Nodejs, Expressjs, MongoDB ( noSQL Database )) or others technologies I could be able to do.</p>
+                  </div>
+                  <div className='banner-img'>
+                      <img src={banner} alt='logo' />
                   </div>
               </div>
-          </div>
-        </header>
+    </header>
   )
 }
 
