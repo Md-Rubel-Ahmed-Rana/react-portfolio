82222ea50c85b35ae1cd5e41649fb34aed88df84
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-20 02:02:38 +0600

    Bugs fixed

--- src/header/nav.js
+++ src/header/nav.js
@@ -15,12 +15,12 @@ function Navbar() {
                   </div>
                   <div>
                       <ul className='navbar-disc'>
-                          <li> Home </li>
+                          <li>Home</li>
                           <li> About </li>
                           <li> Services </li>
                           <li> Blogs </li>
                       </ul>
-                      <div className='collapse' id='collapse'>
+                      <div className='collapse'>
                           <img src={collapseImage} alt='logo' />
                       </div>
                   </div>
@@ -28,8 +28,7 @@ function Navbar() {
           </div>
               <div className='banner'>
                   <div className='banner-info'>
-                      <h3>Future Web Developer</h3>
-                      <h3>About Me</h3>
+                      <h1>Future Web Developer</h1>
                       <p>
                           Hi there, as you see my portfolio you might be understood that I am a Web Developer. I always try to do something to my best. As a web developer, at first I design a UI (user interface) with HTML and CSS and then will convert it to a demandable Website , Web Application or Desktop Application as the client or company requirement by using the most popular technology ( Vanilla JavaScript, Reactjs, TypeScript, Nodejs, Expressjs, MongoDB ( noSQL Database )) or others technologies I could be able to do.</p>
                   </div>
